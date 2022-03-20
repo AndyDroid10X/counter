d29644056e8666a53cd55725ee148428c68ddd36
AndyDroid10X <geekxx10@gmail.com>
2022-03-20 17:53:54 +0500

    change

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
-import counter
+from datetime import date
+
 app = Flask(__name__)
+
+
+def counter():
+    d0 = date(2021, 4, 24)
+    d1 = date.today()
+    return str((d1 - d0).days)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html', count=counter.days_since)
+    return render_template('index.html', count=counter())
 
 
 if __name__ == '__main__':
--- counter.py
+++
@@ -1,5 +0,0 @@
-from datetime import date
-
-d0 = date(2021, 4, 24)
-d1 = date.today()
-days_since = str((d1 - d0).days)
