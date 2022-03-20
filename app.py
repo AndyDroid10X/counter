f9808dec02e4fd6901cf4b5e1fcbba3dea37a360
AndyDroid10X <geekxx10@gmail.com>
2022-03-20 17:41:41 +0500

    schedule

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 import counter
+from apscheduler.schedulers.background import BackgroundScheduler
+
 app = Flask(__name__)
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(counter, 'interval', minutes=720)
+sched.start()
 
 
 @app.route('/')
