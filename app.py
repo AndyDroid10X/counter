84afc88b5470f1281178fec19b6e07238d8e5b2c
AndyDroid10X <geekxx10@gmail.com>
2022-03-22 15:57:23 +0500

    beauty

--- app.py
+++ app.py
@@ -6,24 +6,39 @@ app = Flask(__name__)
 
 year = date.today().year
 if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    diy = 366
+    dpy = 366
 else:
-    diy = 365
+    dpy = 365
 d0 = date(2021, 4, 24)
 d1 = date.today()
 
 
 def days_since():
-    return str((d1 - d0).days)
+    return (d1 - d0).days
 
 
 def days_left():
-    return str((diy - int(days_since())))
+    return dpy - days_since()
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html', d_since=days_since(), d_left=days_left(), d_in_year=diy)
+@app.route("/since")
+def since():
+    return render_template('index.html', main_text=str(days_since()))
+
+
+@app.route("/left")
+def left():
+    return render_template('index.html', main_text=str(days_left()))
+
+
+@app.route("/diy")
+def diy():
+    return render_template('index.html', main_text=str(dpy))
+
+
+@app.route("/")
+def index():  # put application's code here
+    return render_template('index.html', main_text="Press the button")
 
 
 if __name__ == '__main__':
